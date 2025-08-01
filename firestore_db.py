040a7ab9883b988fc9d89c218b4c1e48821eff2e
itsParas000 <jadhavparas740@gmail.com>
2025-08-01 11:00:14 +0530

    Update firestore_db.py

--- firestore_db.py
+++ firestore_db.py
@@ -5,7 +5,7 @@ from firebase_admin import credentials, firestore
 if not firebase_admin._apps:
     cred = credentials.Certificate(dict(st.secrets["service_account"]))
     firebase_admin.initialize_app(cred)
-
+db = firestore.client()
 def add_expense(user_email, date, category, amount, note, username=""):
     expense = {
         "user": user_email,
